40cdf8371eb562d4582629591fc4f780b93ea704
dmmv2r <mcdonoug@hoare7.cs.umsl.edu>
2022-05-05 23:24:40 -0500

    unfinished

--- oss.c
+++ oss.c
@@ -7,23 +7,40 @@
 #include <sys/wait.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
+#include <sys/msg.h>
 #include <time.h>
 
 #define SHM_KEY 52
+#define PERMS 0644
 
 struct shmseg {
    unsigned int seconds;
    unsigned int nanos;
 
-   int instances[10];
+   int instances[10]; //max number of resource
+
+   int pageTable[32];
+   int frameTable[256];
 };
 
-void clearMem(int shmid, struct shmseg *shmp);
+struct msgbuf {
+   int mtype;
+   int message;
+};
+
+void fillInstances(struct shmseg *shmp); //randomly creates maximum number of each resource
+void fillTables(struct shmseg *shmp); //puts -1 into each table location signifying and empty spot
+void clearMem(int shmid, struct shmseg *shmp); //cleans shared memory
 
 int main(int argc, char* argv[]) {
 
    int shmid;
    struct shmseg *shmp;
+
+   struct msgbuf buf;
+   int msgid;
+
+   srand(time(0));
 
    shmid = shmget(SHM_KEY, sizeof(struct shmseg), 0644|IPC_CREAT);
    if(shmid == -1) {
@@ -37,28 +54,44 @@ int main(int argc, char* argv[]) {
    }
 
 
+   if((msgid =  msgget((key_t)12345, 0644 | IPC_CREAT)) == -1) {
+      perror("oss msgget");
+      clearMem(shmid, shmp);
+      return 1;
+   }
+
+   fillTables(shmp);
+   fillInstances(shmp);
+
+   buf.mtype = 1;
+   buf.message = 7; //test message
+
+   if((msgsnd(msgid, &buf, sizeof(buf), 0)) == -1) { //sends message to any child
+      perror("oss msgsnd");
+   }
+
    shmp->seconds = 0;
    shmp->nanos = 0;
 
-   //int *j;
-   //j = 1;
-
    pid_t childpid = 0;
    char* args[] = {"./user_proc", NULL};
-   //char* args[] = {"./user_proc", j, NULL};
 
-
-   if(childpid == fork()) {
-      //printf("execing\n");
+   if(childpid == fork()) { //execs a child process
       execv("./user_proc", args);
    }
 
-   //printf("waiting\n");
-   wait(NULL);
+   wait(NULL); //waits for child to terminate
 
 
-   printf("oss seconds %i\n", shmp->seconds);
-   printf("oss nanos %i\n", shmp->nanos);
+   if((msgrcv(msgid, &buf, sizeof(buf), 0, 0)) == -1) { //receives a message from child process
+      perror("oss msgrcv");
+   }
+
+   printf("oss msg %i\n", buf.message);
+
+   if(msgctl(msgid, IPC_RMID, NULL) == -1) { //cleans message queue
+      perror("msgctl");
+   }
 
    clearMem(shmid, shmp);
 
@@ -83,3 +116,26 @@ void clearMem(int shmid, struct shmseg *shmp) {
    return;
 }
 
+void fillTables(struct shmseg *shmp) {
+   int i;
+   for(i = 0; i < 32; i++) {
+      shmp->pageTable[i] = -1;
+   }
+
+   for(i = 0; i < 256; i++) {
+      shmp->frameTable[i] = -1;
+   }
+
+   return;
+}
+
+void fillInstances(struct shmseg *shmp) {
+   srand(time(0));
+   int i;
+
+   for(i = 0; i < 10; i++) {
+      shmp->instances[i] = (rand() % (20 - 1 + 1)) + 1;
+   }
+
+   return;
+}
--- user_proc.c
+++ user_proc.c
@@ -5,23 +5,41 @@
 #include <sys/wait.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
+#include <sys/msg.h>
 #include <time.h>
 
 #define SHM_KEY 52
+#define PERMS 0644
 
 struct shmseg {
    unsigned int seconds;
    unsigned int nanos;
 
-   int instances[10];
+   int instances[10]; //maximum number of resources possible
+
+   int pageTable[32];
+   int frameTable[256];
 };
 
+struct msgbuf {
+   int mtype;
+   int message;
+};
+
+void fillNeeds(int resourceNeeds[], struct shmseg *shmp); //determines how many of each resources the process needs
 
 int main(int argc, char* argv[]) {
+
+   int resourceNeeds[10];
 
    int shmid;
    struct shmseg *shmp;
    
+   int msgid;
+   struct msgbuf buf;
+
+   srand(time(0));
+
    shmid = shmget(SHM_KEY, sizeof(struct shmseg), 0644|IPC_CREAT);
    if(shmid == -1) {
       perror("user shared memory");
@@ -33,13 +51,77 @@ int main(int argc, char* argv[]) {
       perror("user shared memory attach");
    }
 
-   printf("seconds %i\n", shmp->seconds);
-   printf("nanos %i\n", shmp->nanos);
 
-   shmp->seconds = 8;
-   shmp->nanos = 4;
+   if((msgid = msgget((key_t)12345, 0644 | IPC_CREAT)) == -1) {
+      perror("user msgget");
+      return 1;
+   }
 
+   fillNeeds(resourceNeeds, shmp);
+
+   int j;
+   for(j = 0; j < 10; j++) { //outputs resources needs
+      printf("child 1 needs %i of resource %i\n", resourceNeeds[j], j);
+   }
+
+   int page; //variables for the page and frame finding
+   int frame;
+   int pageEmpty;
+   int frameEmpty;
+   pageEmpty = 0;
+   frameEmpty = 0;
+
+   do { //loops until empty page is found
+      page = (rand() % (32000 - 1 + 1)) + 1; //find random page
+      page = page / 1000;
+
+      if(shmp->pageTable[page] == -1) { //if page location is empty
+
+         do { //loops until empty frame is found
+            frame = (rand() % (256 - 0 + 1)) + 0; //find random frame
+
+            if(shmp->frameTable[frame] == -1) { //if frame location is empty
+               shmp->frameTable[frame] = page;
+               shmp->pageTable[page] = frame;
+               frameEmpty = 1;
+            }
+
+         } while(!frameEmpty);
+
+         pageEmpty = 1;
+      }
+
+   } while(!pageEmpty);
+
+   printf("frame location in page array %i\n", shmp->pageTable[page]);
+   printf("page location in frame array %i\n", shmp->frameTable[frame]);
+
+
+   if((msgrcv(msgid, &buf, sizeof(buf), 0, 0)) == -1) {
+      perror("user msgrcv");
+      return 1;
+   }
+
+   printf("user msg %i\n", buf.message);
+
+   buf.message = 3;
+
+ 
+   if((msgsnd(msgid, &buf, sizeof(buf), 0)) == -1) {
+      perror("user msgsnd");
+   }
 
    printf("leaving user\n");
    return 0;
+}
+
+void fillNeeds(int resourceNeeds[], struct shmseg *shmp) {
+   int i;
+   srand(time(0));
+
+   for(i = 0; i < 10; i++) {
+      resourceNeeds[i] = (rand() % (shmp->instances[i] - 0 + 1)) + 1;
+   }
+
+   return;
 }
